677f9b193ae047ef26445c75ebbe881cdad6418d
FurWaz <fur.waz06@gmail.com>
2021-11-23 00:25:39 +0100

    hand tracking and collision detection

--- client/script.js
+++ client/script.js
@@ -2,10 +2,25 @@ import * as THREE from 'https://unpkg.com/three@0.126.0/build/three.module.js';
 import { VRButton } from 'https://unpkg.com/three@0.126.0/examples/jsm/webxr/VRButton.js';
 import { getModelMaterials, loadModel } from "./js/load.js";
 
+const orderedJoints = [
+    ["thumb-metacarpal", "thumb-phalanx-proximal", "thumb-phalanx-distal", "thumb-tip"],
+    ["index-finger-metacarpal", "index-finger-phalanx-proximal", "index-finger-phalanx-intermediate", "index-finger-phalanx-distal", "index-finger-tip"],
+    ["middle-finger-metacarpal", "middle-finger-phalanx-proximal", "middle-finger-phalanx-intermediate", "middle-finger-phalanx-distal", "middle-finger-tip"],
+    ["ring-finger-metacarpal", "ring-finger-phalanx-proximal", "ring-finger-phalanx-intermediate", "ring-finger-phalanx-distal", "ring-finger-tip"],
+    ["pinky-finger-metacarpal", "pinky-finger-phalanx-proximal", "pinky-finger-phalanx-intermediate", "pinky-finger-phalanx-distal", "pinky-finger-tip"]
+];
+
 let socket = io();
 function log(msg) {
     socket.emit("custom/log", msg);
 }
+
+export var xrRefSpace;
+let boxes_left = [];
+let boxes_right = [];
+let boxMat = new THREE.MeshBasicMaterial({color: 0x00ff00});
+// skeleton for hands
+let boxes = { left: boxes_left, right: boxes_right};
 
 log("Libraries imported successfully");
 
@@ -43,7 +58,7 @@ let renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(render);
-renderer.shadowMap.enabled = true;
+//renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.toneMapping = THREE.ReinhardToneMapping;
 renderer.toneMappingExposure = 2.5;
@@ -52,9 +67,9 @@ document.body.appendChild(renderer.domElement);
 document.body.appendChild(VRButton.createButton(renderer));
 renderer.xr.enabled = true;
 
-
+let foxo = null;
 loadModel('./resources/map.glb', false, true, scene);
-loadModel('./resources/fox.glb', true, true, scene);
+loadModel('./resources/fox.glb', true, true, scene).then(glft => {foxo = glft.scene;});
 loadModel('./resources/seat.glb', true, true, scene).then(model => {getModelMaterials(model)[1].color = new THREE.Color(0x424B63);});
 loadModel('./resources/logo.glb', true, true, scene);
 loadModel('./resources/carpet.glb', true, true, scene).then(model => {getModelMaterials(model)[0].color = new THREE.Color(0x424B63);});
@@ -87,18 +102,15 @@ const AXE_TOUCHPAD_Y = 1;
 const AXE_THUMBSTICK_X = 2;
 const AXE_THUMBSTICK_Y = 3;
 
-function detectCollisions() {
+function detectCollisions(mesh) {
     // cubeBounds.intersectsBox(otherBound);
     // cubeBounds.containsBox(otherBound);
     // cubeBounds.containsPoint(point);
     
-    // let cubeBounds = new THREE.Box3().setFromObject(cube);
-    // let groundBox = new THREE.Mesh(
-    //     new THREE.BoxBufferGeometry(2, 1, 2)
-    // );
-    // groundBox.position.set(0, -0.5, 0);
-    // let groundBound = new THREE.Box3().setFromObject(groundBox);
-    // let result = cubeBounds.intersectsBox(groundBound);
+    let cubeBounds = new THREE.Box3().setFromObject(mesh);
+    let groundBox = foxo;
+    let groundBound = new THREE.Box3().setFromObject(groundBox);
+    return cubeBounds.intersectsBox(groundBound);
 }
 
 function handleController(now, old, dt) {
@@ -114,8 +126,21 @@ function handleController(now, old, dt) {
     }
 }
 
+function addBox(x, y, z, box_list, offset) {
+    var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+    // var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    var cube = new THREE.Mesh( geometry, boxMat );
+    cube.castShadow = true;
+    box_list.push({
+        mesh: cube,
+        position: [x, y, z],
+        offset: offset
+    });
+}
+
 let lastTime = 0, lastLog = 0;
-function render(time) {
+function render(time, frame) {
+    try {
     if (firstTime && renderer.xr.isPresenting) {
         firstTime = false;
         lastTime = time;
@@ -125,6 +150,26 @@ function render(time) {
             models[i] = buildController(source)
             player.add(models[i++]);
         }
+        try {
+            for (const box of boxes_left) {
+                scene.remove(box.mesh);
+            }
+            for (const box of boxes_right) {
+                scene.remove(box.mesh);
+            }
+            boxes_left = [];
+            boxes_right = [];
+            boxes = { left: boxes_left, right: boxes_right};
+            if (XRHand) {
+                for (let i = 0; i <= 24; i++) {
+                    addBox(0, 0, 0, boxes_left, i);
+                    addBox(0, 0, 0,  boxes_right, i);
+                }
+            }
+            session.requestReferenceSpace('bounded-floor').then((refSpace) => {
+                xrRefSpace = refSpace;//.getOffsetReferenceSpace(new XRRigidTransform({x: 0, y: 0, z: 0}));
+            });
+        } catch (err) {log("Error starting xr: "+err);}
     }
     
     let dt = time - lastTime;
@@ -139,35 +184,62 @@ function render(time) {
         camera.lookAt(Math.cos(time*0.001)*.2, 1.1, Math.sin(time*0.001)*.1)
     }
 
-    detectCollisions();
-
     if (session != null) {
-        let i = 0;
-        for (const source of session.inputSources) {
-            let handedness = "right";
-            if (source && source.handedness) {
-                handedness = source.handedness;
+        for (let inputSource of session.inputSources) {
+            if (!inputSource.hand) {
+                continue;
+            } else {
+                let i = 0;
+                for (const finger of orderedJoints) {
+                    for (const joint of finger) {
+                        let box = boxes[inputSource.handedness][i];
+                        let jointPose = null;
+                        if (inputSource.hand[box.offset] !== null) {
+                            jointPose = frame.getJointPose(inputSource.hand.get(joint), xrRefSpace);
+                        }
+                        if (jointPose != null) {
+                            player.add(box.mesh);
+                            box.mesh.position.set(jointPose.transform.position.x, jointPose.transform.position.y, jointPose.transform.position.z);
+                            const q = new THREE.Quaternion(jointPose.transform.orientation.x, jointPose.transform.orientation.y, jointPose.transform.orientation.z, jointPose.transform.orientation.w);
+                            box.mesh.quaternion.copy(q);
+                            box.mesh.scale.set(jointPose.radius, jointPose.radius, jointPose.radius);
+
+                            if (joint == "index-finger-tip") {
+                                if (detectCollisions(box.mesh))
+                                    boxMat.color = new THREE.Color(255, 0, 0);
+                                else boxMat.color = new THREE.Color(0, 255, 0);
+                            }
+
+                        } else {
+                            scene.remove(box.mesh);
+                        }
+                        i++;
+                    }
+                }
             }
-            if (!source.gamepad) continue;
-            const now = {
-                handedness: handedness,
-                buttons: source.gamepad.buttons.map((b) => b.value),
-                axes: source.gamepad.axes,
-                source: source
-            };
-            if (i == old.length) old[i] = now;
-            try {
-                handleController(now, old[i], dt);
-                let controller = renderer.xr.getController(i);
-                models[i].position.set(controller.position.x, controller.position.y, controller.position.z);
-                models[i].rotation.set(controller.rotation.x, controller.rotation.y, controller.rotation.z);
-            } catch (err) {
-                log("Error handling controller: "+err);
-            }
-            old[i] = now;
-            i++;
         }
+        // for (const source of session.inputSources) {
+        //     let handedness = "right";
+        //     if (source && source.handedness) {
+        //         handedness = source.handedness;
+        //     }
+        //     if (!source.gamepad) continue;
+        //     const now = {
+        //         handedness: handedness,
+        //         buttons: source.gamepad.buttons.map((b) => b.value),
+        //         axes: source.gamepad.axes,
+        //         source: source
+        //     };
+        //     if (i == old.length) old[i] = now;
+        //     handleController(now, old[i], dt);
+        //     let controller = renderer.xr.getController(i);
+        //     models[i].position.set(controller.position.x, controller.position.y, controller.position.z);
+        //     models[i].rotation.set(controller.rotation.x, controller.rotation.y, controller.rotation.z);
+        //     old[i] = now;
+        //     i++;
+        // }
     }
+    } catch(err) {log("Error : "+err);}
 
     renderer.render(scene, camera);
 }
